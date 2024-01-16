75518e1f7362fa1a84c1f9b9bab76c8dbee44d81
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2024-01-16 19:45:53 +0530

    feat: :zap: two crystal problem fix issue

--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -2,7 +2,7 @@ export default function two_crystal_balls(breaks: boolean[]): number {
     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
     let i = jmpAmount;
-    for (; i < breaks.length; i++) {
+    for (; i < breaks.length; i += jmpAmount) {
         if (breaks[i]) {
             break;
         }
@@ -13,6 +13,5 @@ export default function two_crystal_balls(breaks: boolean[]): number {
             return i;
         }
     }
-
     return -1;
 }
