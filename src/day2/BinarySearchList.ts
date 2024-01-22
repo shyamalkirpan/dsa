05b5a7ddfeb29f9ca9a9a3e7f3ad050a4b18ba99
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2024-01-22 23:58:01 +0530

    feat: :zap:

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,7 +1,6 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
-    //low+(high-low)/2
     do {
         const m = Math.floor(lo + (hi - lo) / 2);
         const v = haystack[m];
