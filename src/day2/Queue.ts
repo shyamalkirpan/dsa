b50123788833fd20baacac1c49a407a16387aae6
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2024-01-23 23:20:31 +0530

    feat: :zap: queue

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,13 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Queue<T> {
     public length: number;
 
-    
+    constructor() {}
 
-    constructor() {
-    }
-
-    enqueue(item: T): void {
-
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    enqueue(item: T): void {}
+    deque(): T | undefined {}
+    peek(): T | undefined {}
 }
