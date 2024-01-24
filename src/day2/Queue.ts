83e4f618ac7d6dc5fe11777c4cca0fbf9ca72a33
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2024-01-24 13:22:22 +0530

    feat: :zap: queue

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -4,10 +4,44 @@ type Node<T> = {
 };
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
